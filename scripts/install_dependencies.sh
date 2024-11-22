85a11f344ac17eaa9c082c35104e9f28a8624874
Sumer Ali <96568081+Sumerali5581@users.noreply.github.com>
2024-11-22 14:25:38 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-sudo yum update -y
+
+# Update packages and resolve conflicts
+sudo yum update --best --allowerasing -y
+
+# Install Python 3 and pip
 sudo yum install python3 -y
+sudo yum install python3-pip -y
+
+# Install Flask
 pip3 install flask
