b04c5ebced326f0e2a4d674e6b311ffc4ddff4a4
Sumer Ali <96568081+Sumerali5581@users.noreply.github.com>
2024-11-23 11:02:42 +0530

    new

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello,This is Your Flask app is successfully deployed!"
+    return "Hello, Your Flask app is successfully deployed!"
+
+@app.route("/one")
+def one():
+    return "Hello"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
